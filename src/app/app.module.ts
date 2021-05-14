deb2d7957f7b4f697ccb3da0dff0550addbf3953
Iramar Vasquez <iramarvasquez@gmail.com>
2021-05-13 22:49:47 -0300

    Adding Bootstrap and a test button to the interface component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { CartComponent } from './components/cart/cart.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
