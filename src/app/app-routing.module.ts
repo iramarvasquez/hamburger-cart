f66f112c8de12e664ebe9fc99633aa655660be48
Iramar Vasquez <iramarvasquez@gmail.com>
2021-05-18 03:29:56 -0300

    Testing a product list format in HTML

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
 import { InterfaceComponent } from './components/interface/interface.component';
 import { ProductsComponent } from './components/products/products.component';
 
 const routes: Routes = [
   { path: '', component: InterfaceComponent},
   { path: 'products', component: ProductsComponent},
+  { path: 'carrito', component: CartComponent},
 ];
 
 @NgModule({
